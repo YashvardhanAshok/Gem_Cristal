57b393d3b61dd4103ce296800be3e25fdd98e132
Yash <104421123+YashvardhanAshok@users.noreply.github.com>
2025-05-06 17:51:49 +0530

    .

--- try.py
+++ try.py
@@ -6,8 +6,9 @@ from openpyxl.styles import Font, PatternFill
 
 # Define paths
 base_dir = os.path.dirname(__file__)
-json_path = os.path.join(base_dir, 'website', 'json', 'gem.json')
-excel_path = os.path.join(base_dir, 'website', 'json', 'gem.xlsx')
+# json_path = os.path.join(base_dir, 'website', 'json', 'gem.json')
+json_path = os.path.join(os.path.dirname(__file__), 'db',"json", 'MINISTRY OF HOME AFFAIRS.json')
+excel_path = os.path.join(base_dir, 'db',"json", 'MINISTRY OF HOME AFFAIRS.xlsx')
 
 with open(json_path, 'r', encoding='utf-8') as f:
     data = pd.read_json(f)
@@ -19,9 +20,8 @@ data.to_excel(excel_path, index=False)
 # import os
 # import json
 
-# # Define the product categories
 # product = [
-#   ["Electric Wires/Cable", "A.C Static Meter", "Lightning Arrestor", "Miniature Circuit Breaker Switches"],
+#   ["Electric Cable",'"Electric Wires', "A.C Static Meter", "Lightning Arrestor", "Miniature Circuit Breaker Switches"],
 #   ["Solar Street Light", "Solar Power Plant", "Solar Water Heater", "Solar Lantern", "Solar Battery"],
 #   ["Roti Making Machine", "Milk Boiler", "Dough Kneader", "Bain Marie", "Commercial Mixer", "Wet Grinder", "Vegetable Cutter", "Rice Boiler", "Idli Steamer", "Oven", "Tandoor", "Water Dispenser", "Water Cooling"],
 #   ["Hand Held Gas Detector", "Under Water Torch"],
@@ -41,8 +41,8 @@ data.to_excel(excel_path, index=False)
 # # Flatten product list
 # flat_products = [item.lower() for sublist in product for item in sublist]
 
-# # JSON path
-# json_path = os.path.join(os.path.dirname(__file__), 'website', 'json', 'gem.json')
+# # json_path = os.path.join(os.path.dirname(__file__), 'website', 'json', 'gem.json')
+# json_path = os.path.join(os.path.dirname(__file__), 'db',"json", 'MINISTRY OF HOME AFFAIRS.json')
 
 # # Load JSON file
 # with open(json_path, 'r', encoding='utf-8') as f:
